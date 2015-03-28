9e987565a213b40f6b19b06f7e62bcbb2dac8587
Bruce Sutherland <bruce@brucesutherland.co.uk>
2015-03-28 17:01:37 +1100

    Update Recipe1-5 to use an auto string

--- Recipe1-5/HelloWorld.cpp
+++ Recipe1-5/HelloWorld.cpp
@@ -5,7 +5,7 @@ int main(void)
 {
     using namespace std::string_literals;
 
-    std::string output = "Hello World!"s;
+    auto output = "Hello World!"s;
     std::cout << output << std::endl;
 
     return 0;
