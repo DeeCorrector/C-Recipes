0b7f8e01a38a53d9065fbc4384465467d3adfc67
Bruce Sutherland <bruce@brucesutherland.co.uk>
2015-04-05 19:43:45 +1000

    Update Listing 2-10.

--- Recipe2-3/Listing2-10/main.cpp
+++ Recipe2-3/Listing2-10/main.cpp
@@ -12,5 +12,8 @@ int main()
     auto variable{ 1 };
     cout << "Type of variable: " << typeid(variable).name() << endl;
 
+    auto variable2{ MyClass{} };
+    cout << "Type of variable: " << typeid(variable2).name() << endl;
+
     return 0;
 }
